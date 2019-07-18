ad8a22b11d8b0915b488cb3c5622c7111ace8758
LuossG <1977427767@qq.com>
2019-07-18 09:43:07 +0800

    更新暴击，冲锋技能和技能冷却

--- python/作业/2019_07_17.py
+++ python/作业/2019_07_17.py
@@ -18,15 +18,27 @@ def roll(probability):
 
 #英雄行动
 def heroAction(theActor):
+    sk1 = True
     tmp = dict(list(theActor)[0])
     print("你的回合，请选择你的行动：")
-    print(f"1.攻击\t\t\t2.{tmp[role[4]]}")
-    input1 = int(input())
-    if input1 == 1:
-        gongJi(theActor[0],theActor[1])
-    elif input1 == 2:
-        skills[tmp[role[4]]](theActor[0],theActor[1])
+    print(f"1.攻击\t\t\t", end = "")
+    if tmp[role[6]] > 0:
+        print(f"  {tmp[role[4]]}（{tmp[role[6]]}回合后可使用）", end = "")
+        sk1 = False
+    else :
+        print(f"2.{tmp[role[4]]}", end = "")
+    print()
+    while True:
+        input1 = int(input())
+        if input1 == 1:
+            gongJi(theActor[0],theActor[1])
+            break
+        elif sk1 and input1 == 2:
+            skills[tmp[role[4]]](theActor[0],theActor[1])
+            break
+        print("请输入正确的选项！")
     pass
+
 #怪物行动
 def monsterAction(theActor):
     tmp = dict(list(theActor)[0])
@@ -50,13 +62,12 @@ def roundStart(theActor):
         print(f"{theActor[heIn][role[0]]}\t\t\t\t\t\t{theActor[1-heIn][role[0]]}")
         print(f"{theActor[heIn][role[1]]}---------VS---------{theActor[1-heIn][role[1]]}")
         tmp[role[8]](theActor)
-    updataStatus(tmp)
 
 #回合开始函数
 def roundEnd(theActor):
-    theActor.reverse()
     updataStatus(theActor[0])
-    updataStatus(theActor[1])
+    # updataStatus(theActor[1])
+    theActor.reverse()
 
 #更新角色状态
 def updataStatus(theRole):
@@ -73,12 +84,24 @@ def battleEnd(theRole):
     tmp = theRole[1] if theRole[0] == winner else theRole[0]
     print(f"{winner[role[0]]}击败了{tmp[role[0]]}!")
 
+#技能冷却函数
+def skillCooling(theRole,skillName):
+    if theRole[role[4]] == skillName:
+        theRole[role[6]] = coolingTime[skillName]
+    else :
+        theRole[role[7]] = coolingTime[skillName]
+
 #冲锋函数
 def chongFeng(attacker, Attackee):
     hurtVal = attacker[role[2]]+10
     reduceBlood(Attackee, hurtVal)
     print(f"{attacker[role[0]]}对{Attackee[role[0]]}使用了冲锋！对{Attackee[role[0]]}造成了{hurtVal}点伤害！")
+    if roll(30):
+        Attackee[role[3]] = 1
+        print(f"{attacker[role[0]]}释放的冲锋击倒了{Attackee[role[0]]}！持续一回合！")
+    skillCooling(attacker,"冲锋")
     pass
+
 #爆发函数
 def baoFa(attacker, Attackee):
     pass
@@ -96,8 +119,13 @@ def yinBi(attacker, Attackee):
     pass
 #攻击函数
 def gongJi(attacker, Attackee):
-    reduceBlood(Attackee, attacker[role[2]])
-    print(f"{attacker[role[0]]}攻击了{Attackee[role[0]]}，对{Attackee[role[0]]}造成了{attacker[role[2]]}点伤害")
+    hurtVal = attacker[role[2]] + 10
+    print(f"{attacker[role[0]]}攻击了{Attackee[role[0]]}！",end = "")
+    if roll(attacker[role[9]]):
+        hurtVal *= 2
+        print(f"{attacker[role[0]]}打出了暴击，效果拔群！",end = "")
+    print(f"对{Attackee[role[0]]}造成了{hurtVal}点伤害!")
+    reduceBlood(Attackee, hurtVal)
     pass
 
 #基础角色数据结构：
@@ -110,15 +138,20 @@ def gongJi(attacker, Attackee):
 #6.sk1st      技能1状态
 #7.sk2st      技能2状态     技能状态：值为非负数，0表示可以释放, 非零数为当前回合该技能的冷却时间
 #8.funact     行动函数
-role = ("name", "hp", "atk", "state", "skill1", "skill2", "sk1st", "sk2st", "funact")
+#9.crit       暴击率
+role = ("name", "hp", "atk", "state", "skill1", "skill2", "sk1st", "sk2st", "funact", "crit")
 
 hero = {role[0]:"无名氏", role[3]:0, role[8]:heroAction, role[6]:0, role[7]:0}
 
-occupations = [{"occ":"战士", "atk":8, "hp":200, "skill1":"冲锋", "skill2":"爆发"},
-               {"occ":"法师", "atk":3, "hp":100, "skill1":"火球术", "skill2":"冰封术"},
-               {"occ":"盗贼", "atk":13, "hp":80,  "skill1":"连刺", "skill2":"隐蔽"}]
-skills = {"冲锋":chongFeng, "爆发":baoFa, "火球术":huoQiuShu, "冰封术":bingFengShu, "连刺":LianCi, "隐蔽":yinBi, "冲锋":chongFeng, "攻击":gongJi}
-monsters = [{role[0]:"皮卡耗子", role[1]:100, role[2]:3, role[3]:0, role[4]:"攻击", role[5]:"攻击", role[6]:0, role[7]:0, role[8]:monsterAction}]
+occupations = [{"occ":"战士", "atk":8, "hp":200, "skill1":"冲锋", "skill2":"爆发", "crit":10},
+               {"occ":"法师", "atk":3, "hp":100, "skill1":"火球术", "skill2":"冰封术", "crit":8},
+               {"occ":"盗贼", "atk":13, "hp":80,  "skill1":"连刺", "skill2":"隐蔽", "crit":20}]
+
+skills = {"冲锋":chongFeng, "爆发":baoFa, "火球术":huoQiuShu, "冰封术":bingFengShu, "连刺":LianCi, "隐蔽":yinBi, "攻击":gongJi}
+
+coolingTime = {"冲锋":2, "爆发":5, "火球术":1, "冰封术":3, "连刺":3, "隐蔽":2, "攻击":0}
+
+monsters = [{role[0]:"皮卡耗子", role[1]:200, role[2]:3, role[3]:0, role[4]:"攻击", role[5]:"攻击", role[6]:0, role[7]:0, role[8]:monsterAction, role[9]:15}]
 
 # 选择职业
 # 设置姓名
@@ -128,8 +161,8 @@ print("职业列表：")
 for i in range(len(occupations)):
     print(f"{i}.", end = "\t")
     print(occupations[i]["occ"], end = "\t")
-    print(f"hp：{occupations[i][role[1]]}",  end = "\t")
-    print(f"攻击力：%03d", end="\t\t")
+    print(f"hp：%03d"%occupations[i][role[1]],  end = "\t")
+    print(f"攻击力：%03d"%occupations[i][role[2]], end="\t\t")
     print(f"技能：{occupations[i][role[4]]}",  end="\n")
 if int(input("请选择职业(输入序号)：")) != 0:
     print("抱歉，当前只能够选择战士")
@@ -139,7 +172,7 @@ hero["name"] = input("请决定您的英雄名：")
 testPrint(hero)
 
 print("您的英雄信息如下：")
-print(f"名字：{hero[role[0]]}\t职业：{hero['occ']}\t生命值：{hero[role[1]]}\t攻击力：{hero[role[2]]}\t技能：{hero[role[4]]}")
+print(f"名字：{hero[role[0]]}\t职业：{hero['occ']}\t生命值：{hero[role[1]]}\t攻击力：%03d\t技能：{hero[role[4]]}"%hero[role[2]])
 
 #战斗流程：
 #   回合开始
@@ -148,14 +181,12 @@ print(f"名字：{hero[role[0]]}\t职业：{hero['occ']}\t生命值：{hero[role
 #       分为怪兽回合与英雄回合，调用对应的回合行动函数
 #   回合结束
 #       显示双方状态，修改状态
-theMoster = monsters[random.randint(len(monsters))];
+theMoster = monsters[random.randint(0,len(monsters)-1)]
 actors = [hero.copy(), theMoster.copy()]
 testPrint(actors[0])
 testPrint(actors[1])
 print(f"你遭遇到了{actors[1]['name']}，开始战斗！")
-while True:
+while not check(actors):
     roundStart(actors)
     roundEnd(actors)
-    if check(actors):
-        break;
 battleEnd(actors)
